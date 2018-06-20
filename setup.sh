bd880b87567cb5379282208ff3d874bbd71f1451
Zeyu Chen <chenzy@gatech.edu>
2018-06-20 14:39:17 -0700

    Set up.

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# !/bin/bash
+#
+
+# Install Django
+sudo pip3 install Django
+
+# Install MySQL
+sudo apt-get install mysql-server
+sudo apt-get install libmysqlclient-dev
+sudo pip3 install mysqlclient
