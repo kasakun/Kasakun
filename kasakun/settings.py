822acc9577cefd6387dad147381d0e11a4c26e4b
Zeyu Chen <chenzy@gatech.edu>
2018-07-09 23:43:49 -0700

    admin static migrate

--- kasakun/settings.py
+++ kasakun/settings.py
@@ -31,13 +31,13 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'czy-kasakun.com']
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'website.apps.WebsiteConfig',
+    'django.contrib.admin',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +123,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
